f2629a28f6e34d83f752675c254ca2035b54ed29
Yaron Serlin <yaron155@gmail.com>
2025-07-12 18:08:30 +0300

    Add code and closedAt fields to Fault schema

--- backend/models/Fault.js
+++ backend/models/Fault.js
@@ -2,11 +2,13 @@
 const mongoose = require('mongoose');
 
 const FaultSchema = new mongoose.Schema({
+    code: { type: String, },
     tool: { type: mongoose.Schema.Types.ObjectId, ref: 'Tool', required: true },
     operator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     description: { type: String, required: true },
     photos: [{ type: String }],
     status: { type: String, enum: ['open', 'closed'], default: 'open' },
+    closedAt: { type: Date },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Fault', FaultSchema);
