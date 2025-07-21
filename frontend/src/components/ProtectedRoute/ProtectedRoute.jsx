9061c82d66f8193291e98d9edf3b8e110f410cc5
Yaron Serlin <yaron155@gmail.com>
2025-07-21 18:20:44 +0300

    use loading component while load

--- frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
+import LoadingComponent from '../LoadingComponent/LoadingComponent';
 
 /**
  * Wraps protected routes, redirecting unauthenticated users to login
@@ -12,7 +13,7 @@ const ProtectedRoute = ({ children }) => {
 
     if (loading) {
         // You can replace this with a spinner component
-        return <div>Loading...</div>;
+        return <LoadingComponent />;
     }
 
     if (!user) {
