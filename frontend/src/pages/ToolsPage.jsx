65b11a58cd4cf16eb26925208606c5709ed449c6
Yaron Serlin <yaron155@gmail.com>
2025-07-31 23:38:02 +0300

    refactor ToolsPage to use ToolContext for state management

--- frontend/src/pages/ToolsPage.jsx
+++ frontend/src/pages/ToolsPage.jsx
@@ -3,47 +3,19 @@ import React, { useEffect, useState } from 'react';
 import {
     Container,
     Typography,
-    CircularProgress,
-    Button,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
 } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import toolsService from '../services/toolsService';
+
 import ToolsList from '../components/Tool/ToolsList/ToolsList';
 import LoadingComponent from '../components/LoadingComponent/LoadingComponent';
 import ErrorComponent from '../components/ErrorComponent/ErrorComponent';
+import { useTool } from '../contexts/ToolContext';
 
 /**
  * Displays a list of tools and provides modals for create, update, and delete.
  */
 export default function ToolsPage() {
-    const [tools, setTools] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    const sortBySerial = (arr) =>
-        [...arr].sort((a, b) =>
-            // If a or b is undefined, treat it as less than the other
-            (a.localSerialNumber || '').localeCompare(b.localSerialNumber || '') ||
-            // If localSerialNumber is the same, sort by name
-            a.name.localeCompare(b.name)
-
-
-        );
-
-    useEffect(() => {
-        toolsService
-            .getAll()
-            .then((data) => setTools(sortBySerial(data)))
-            .catch((err) => {
-                console.error(err);
-                setError('Failed to load tools');
-            })
-            .finally(() => setLoading(false));
-    }, []);
+    // const { tools } = useTool();
+    const { tools, loading, error } = useTool();
 
     if (error) {
         return (
